b92afdddbe315ebe4e03b7362093269643cc5de1
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-01-12 23:03:17 +0430

    cleanup

--- svelite.config.js
+++ svelite.config.js
@@ -2,221 +2,9 @@ import { AdminPanelPlugin } from 'svelite-admin'
 import * as modules from './modules'
 
 const collections = [
-    {
-        "name": "Products",
-        "slug": "products",
-        "icon": "shopping-cart",
-        "fields": [
-            {
-                "name": "name",
-                "label": "Product Name",
-                "type": "plain_text",
-                "tab": "General",
-                "visibility": {
-                    "list": true,
-                    "create": true,
-                    "update": true
-                }
-            },
-            {
-                "name": "description",
-                "label": "Product Description",
-                "type": "rich_text",
-                "tab": "General",
-                "visibility": {
-                    "list": true,
-                    "create": true,
-                    "update": true
-                }
-            },
-            {
-                "name": "price",
-                "label": "Price",
-                "type": "number",
-                "tab": "General",
-                "visibility": {
-                    "list": true,
-                    "create": true,
-                    "update": true
-                }
-            },
-            {
-                "name": "images",
-                "label": "Product Images",
-                "type": "image",
-                "tab": "Media",
-                "visibility": {
-                    "list": true,
-                    "create": true,
-                    "update": true
-                }
-            },
-            {
-                "name": "category",
-                "label": "Category",
-                "type": "relation",
-                "tab": "Categories",
-                "visibility": {
-                    "list": true,
-                    "create": true,
-                    "update": true
-                }
-            }
-        ]
-    },
-    {
-        "name": "Categories",
-        "slug": "categories",
-        "icon": "category",
-        "fields": [
-            {
-                "name": "name",
-                "label": "Category Name",
-                "type": "plain_text",
-                "tab": "General",
-                "visibility": {
-                    "list": true,
-                    "create": true,
-                    "update": true
-                }
-            },
-            {
-                "name": "description",
-                "label": "Category Description",
-                "type": "rich_text",
-                "tab": "General",
-                "visibility": {
-                    "list": true,
-                    "create": true,
-                    "update": true
-                }
-            },
-            {
-                "name": "parent",
-                "label": "Parent Category",
-                "type": "relation",
-                "tab": "Categories",
-                "visibility": {
-                    "list": true,
-                    "create": true,
-                    "update": true
-                }
-            }
-        ]
-    },
-    {
-        "name": "Orders",
-        "slug": "orders",
-        "icon": "shopping-cart",
-        // "sidebar": (context) => context.user.role === 'admin' || context.user.role === 'seller',
-        "fields": [
-            {
-                "name": "user",
-                "label": "User",
-                "type": "relation",
-                "tab": "General",
-                "visibility": {
-                    "list": true,
-                    "create": true,
-                    "update": true
-                }
-            },
-            {
-                "name": "products",
-                "label": "Products",
-                "type": "relation",
-                "tab": "General",
-                "visibility": {
-                    "list": true,
-                    "create": true,
-                    "update": true
-                }
-            },
-            {
-                "name": "status",
-                "label": "Status",
-                "type": "select",
-                "tab": "General",
-                "visibility": {
-                    "list": true,
-                    "create": true,
-                    "update": true
-                },
-                "items": [
-                    {
-                        "key": "pending",
-                        "value": "Pending"
-                    },
-                    {
-                        "key": "processing",
-                        "value": "Processing"
-                    },
-                    {
-                        "key": "shipped",
-                        "value": "Shipped"
-                    },
-                    {
-                        "key": "delivered",
-                        "value": "Delivered"
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        "name": "Users",
-        "slug": "users",
-        "icon": "user",
-        "fields": [
-            {
-                "name": "name",
-                "label": "Name",
-                "type": "plain_text",
-                "tab": "General",
-                "visibility": {
-                    "list": true,
-                    "create": true,
-                    "update": true
-                }
-            },
-            {
-                "name": "email",
-                "label": "Email",
-                "type": "plain_text",
-                "tab": "General",
-                "visibility": {
-                    "list": true,
-                    "create": true,
-                    "update": true
-                }
-            },
-            {
-                "name": "role",
-                "label": "Role",
-                "type": "select",
-                "tab": "General",
-                "visibility": {
-                    "list": true,
-                    "create": true,
-                    "update": true
-                },
-                "items": [
-                    {
-                        "key": "admin",
-                        "value": "Admin"
-                    },
-                    {
-                        "key": "seller",
-                        "value": "Seller"
-                    },
-                    {
-                        "key": "customer",
-                        "value": "Customer"
-                    }
-                ]
-            }
-        ]
-    }
+    // collection definition..
+    // You can use chatgpt_prompt and change ## Prompt section 
+    // to build initial structure of your data models.
 ]
 
 export default {
@@ -224,16 +12,14 @@ export default {
        AdminPanelPlugin({
            collections,
            logo: '',
-           theme: 'dark',
+           theme: 'light', // 'dark',
            dir: 'ltr',
            modules
             // admin panel config...
          }),
     ],
     // custom modules
-    modules: {
-
-    },
+    modules: {},
     // custom layouts
     layouts: {},
     pages: [
@@ -250,9 +36,4 @@ export default {
         }
     ],
     api: 'http://localhost:3010/api'
-}
-
-export const config = {
-    adapter: 'auto'
-
 }
